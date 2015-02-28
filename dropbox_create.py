a0a0bbe9ec46d7182cef0c0d9bfcf54649250d58
Adam Cohen <adamcohen578@gmail.com>
2015-02-28 13:00:27 -0600

    Fixing tabs.

--- dropbox_create.py
+++ dropbox_create.py
@@ -26,51 +26,51 @@ def monkeypatch_mechanize():
         SubmitControl.__init__ = __init__
 
 def setUpBrowser():
-	monkeypatch_mechanize()
-	br = mechanize.Browser()
-	cj = cookielib.LWPCookieJar()
-	br.set_cookiejar(cj)
+    monkeypatch_mechanize()
+    br = mechanize.Browser()
+    cj = cookielib.LWPCookieJar()
+    br.set_cookiejar(cj)
 
-	# Browser options
-	br.set_handle_equiv(True)
-	br.set_handle_gzip(True)
-	br.set_handle_redirect(True)
-	br.set_handle_referer(True)
-	br.set_handle_robots(False)
+    # Browser options
+    br.set_handle_equiv(True)
+    br.set_handle_gzip(True)
+    br.set_handle_redirect(True)
+    br.set_handle_referer(True)
+    br.set_handle_robots(False)
 
-	# Follows refresh 0 but not hangs on refresh > 0
-	br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+    # Follows refresh 0 but not hangs on refresh > 0
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
-	# Want debugging messages?
-	#br.set_debug_http(True)
-	#br.set_debug_redirects(True)
-	#br.set_debug_responses(True)
+    # Want debugging messages?
+    #br.set_debug_http(True)
+    #br.set_debug_redirects(True)
+    #br.set_debug_responses(True)
 
-	br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-	return br
-	
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    return br
+    
 
 def createDropbox(br, fname, lname, email, password):
-	br.open("https://www.dropbox.com/login")
-	br.select_form(nr=2)
-	br.form.set_value("varun", "fname")
-	br.form.set_value("berry", "lname")
-	br.form.set_value("uiuclotsbox.@gmail.com", name="email")
-	br.form.set_value("berry123", "password")
-	br.form.find_control("tos_agree").items[0].selected=True
-	br.submit()
+    br.open("https://www.dropbox.com/login")
+    br.select_form(nr=2)
+    br.form.set_value(fname, "fname")
+    br.form.set_value(lname, "lname")
+    br.form.set_value(email, name="email")
+    br.form.set_value(password, "password")
+    br.form.find_control("tos_agree").items[0].selected=True
+    br.submit()
 
 def createApp(br, appname, fileTypes=[0, 1, 2])
-	br.open("https://www.dropbox.com/developers/apps/create")
-	br.select_form(nr=0)
-	br.form.set_value(["api"], name="app_type")
-	br.form.set_value(["files"], name="data_type")
-	br.form.set_value(["specific"], name="file_access")
-	br.form.set_value(appname, name="name")
-	for type in fileTypes:
-		br.form.find_control("file_types").items[type].selected=True
-		br.form.find_control("file_types").items[type].selected=True
-		br.form.find_control("file_types").items[type].selected=True
-	br.form.find_control("tos_accept").items[0].selected=True
-	br.submit()
+    br.open("https://www.dropbox.com/developers/apps/create")
+    br.select_form(nr=0)
+    br.form.set_value(["api"], name="app_type")
+    br.form.set_value(["files"], name="data_type")
+    br.form.set_value(["specific"], name="file_access")
+    br.form.set_value(appname, name="name")
+    for type in fileTypes:
+        br.form.find_control("file_types").items[type].selected=True
+        br.form.find_control("file_types").items[type].selected=True
+        br.form.find_control("file_types").items[type].selected=True
+    br.form.find_control("tos_accept").items[0].selected=True
+    br.submit()
 
