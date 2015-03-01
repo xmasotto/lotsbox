9d9e1804e576011dedf85dfb3a161de08b6e7485
timthejiang <timthejiang@gmail.com>
2015-02-28 23:05:47 -0600

    Rename index.html to sign_in.html

--- website.py
+++ website.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-	return render_template('index.html')
+@app.route('/signin')
+def sign_in():
+  return render_template('sign_in.html')
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
