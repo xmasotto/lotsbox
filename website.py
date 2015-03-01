3448f5d813b8952f1c9cf0cbf588cfdedb1d55b8
timthejiang <timthejiang@gmail.com>
2015-02-28 23:07:56 -0600

    Add main webpage - no backend functionality

--- website.py
+++ website.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def main():
+  return render_template('main.html')
+
 @app.route('/signin')
 def sign_in():
   return render_template('sign_in.html')
